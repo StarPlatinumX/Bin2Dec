ae46943dd336b0affb05f36226ba7c8f78cb743a
StarPlatinumX <luigarmor2@gmail.com>
2020-02-12 19:17:35 +0100

    Bin2Dec

--- src/Bin2Dec.java
+++ src/Bin2Dec.java
@@ -1,8 +1,12 @@
 
 public class Bin2Dec {
 
-	public static int bin2Dec(byte input) {
-		int res = input;
+	public static int bin2Dec(String input) {
+		int res = 0;
+		for(int i = 0; i < input.length(); i++) {
+			int pow = (int) Math.pow(2, i);
+			res += Character.getNumericValue(input.charAt(input.length()-1-i))*pow;
+		}
 		return res;
 	}
 }
